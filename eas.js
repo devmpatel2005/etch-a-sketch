ca1e91016ee546e912f7f25a078348efccbd377e
devmpatel2005 <devmpatel2005@gmail.com>
2025-03-01 23:08:19 -0500

    Button to change size added

--- eas.js
+++ eas.js
@@ -1,22 +1,34 @@
 const container = document.getElementById("container");
 
-let size = prompt("Choose a grid size from 1 to 100");
-createGrid(size);
+createGrid(16);
 
 function createGrid(size){
+    container.innerHTML = '';
     for(let i = 0; i < size * size; i++){
         const div = document.createElement("div");
         div.classList.add("grid-item");
         div.style.width = (960 / size) + "px";
         div.style.height = (960 / size) + "px";
         container.appendChild(div);
+
+        const boxes = document.querySelectorAll(".grid-item");
+        boxes.forEach((box) => {
+            box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "black";
+            });
+        });
     }
 }
+const changeSize = document.createElement("button");
+changeSize.textContent = "Change Size";
+document.body.appendChild(changeSize);
 
-const boxes = document.querySelectorAll(".grid-item");
-
-boxes.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "black";
-    });
+changeSize.addEventListener("click", () => {
+    let size = prompt("Choose a grid size from 1 to 100");
+    createGrid(size);
 });
+
+
+
+
+
