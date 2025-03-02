4e1754cf034a0c442cd9167a352dfe3a15f7d579
devmpatel2005 <devmpatel2005@gmail.com>
2025-03-01 22:45:34 -0500

    Can change size of grid

--- eas.js
+++ eas.js
@@ -1,8 +1,22 @@
 const container = document.getElementById("container");
 
-for(let i = 0; i < 16 * 16; i++){
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
-    container.appendChild(div);
+let size = prompt("Choose a grid size from 1 to 100");
+createGrid(size);
+
+function createGrid(size){
+    for(let i = 0; i < size * size; i++){
+        const div = document.createElement("div");
+        div.classList.add("grid-item");
+        div.style.width = (960 / size) + "px";
+        div.style.height = (960 / size) + "px";
+        container.appendChild(div);
+    }
 }
 
+const boxes = document.querySelectorAll(".grid-item");
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "black";
+    });
+});
